c5ce08f4f00f90bb7971393b3f1eb10c39876456
Yanfe Pedroza Alberto <38539546+Yanfep@users.noreply.github.com>
2023-06-19 19:34:17 +0000

    V3

--- src/app.js
+++ src/app.js
@@ -6,34 +6,41 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = () => {
-  document.querySelector("#btn").addEventListener("click", () => {
-    document.querySelector("#the-excuse").innerHTML = generateExcuse();
-  });
-  console.log("Hello Rigo from the console! ");
-};
-
-let generateExcuse = () => {
   let pronoun = ["A", "The"];
   let subject = ["jogger", "racoo", "dog", "driver", "comedian", "pincone"];
   let action = ["took my", "threw my", "yelled at my", "stole my", "bit my"];
   let possetion = ["homework", "toe", "car", "shoe"];
   let where = ["on the street", "in my house", "in my driveway"];
 
-  let proIndx = Math.floor(Math.random() * pronoun.length);
-  let subjIndx = Math.floor(Math.random() * subject.length);
-  let actionIndex = Math.floor(Math.random() * action.length);
-  let possetionIndex = Math.floor(Math.random() * possetion.length);
-  let whereIndex = Math.floor(Math.random() * where.length);
+  function generateExcuse(array1, array2, array3, array4, array5) {
+    let proIndx = Math.floor(Math.random() * array1.length);
+    let subjIndx = Math.floor(Math.random() * array2.length);
+    let actionIndex = Math.floor(Math.random() * array3.length);
+    let possetionIndex = Math.floor(Math.random() * array4.length);
+    let whereIndex = Math.floor(Math.random() * array5.length);
 
-  return (
-    pronoun[proIndx] +
-    " " +
-    subject[subjIndx] +
-    " " +
-    action[actionIndex] +
-    " " +
-    possetion[possetionIndex] +
-    " " +
-    where[whereIndex]
+    let var1 = pronoun[proIndx];
+    let var2 = subject[subjIndx];
+    let var3 = action[actionIndex];
+    let var4 = possetion[possetionIndex];
+    let var5 = where[whereIndex];
+
+    let concatenacionArray =
+      var1 + " " + var2 + " " + var3 + " " + var4 + " " + var5;
+
+    return concatenacionArray;
+  }
+
+  let concatenacionFinal = generateExcuse(
+    pronoun,
+    subject,
+    action,
+    possetion,
+    where
   );
+
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = concatenacionFinal;
+  });
+  console.log("Hello Rigo from the console! ");
 };
